239760d81ebad83a9991511ed9e64e3c96ea16fd
joyfuldev <wings2132@naver.com>
2021-03-07 17:40:48 +0900

    MVC Pattern 3

---
+++ controller/videoController.js
@@ -0,0 +1,7 @@
+export const home = (req, res) => res.send("Home");
+export const search = (req, res) => res.send("Search");
+export const videos = (req, res) => res.sent("Videos");
+export const upload = (req, res) => res.sent("Upload");
+export const videoDetail = (req, res) => res.sent("Video Detail");
+export const editVideo = (req, res) => res.sent("Edit Video");
+export const deleteVideo = (req, res) => res.sent("Delete Video");
