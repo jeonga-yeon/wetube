8d32ab779864ee2d7ad03b7bfff1077c18609436
joyfuldev <wings2132@naver.com>
2021-03-09 17:58:15 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,46 @@
+export const videos = [
+    {
+        id:234254,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Joy"
+        }
+    },
+    {
+        id:123456,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Joy"
+        }
+    },
+    {
+        id:567890,
+        title: "Video nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Joy"
+        }
+    },
+    {
+        id:787878,
+        title: "Video perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Joy"
+        }
+    }
+]
