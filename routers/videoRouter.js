08daed50afabd85c57f252b849067c2d7a3eb8cb
joyfuldev <wings2132@naver.com>
2021-03-24 19:26:01 +0900

    Editing a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,15 +1,22 @@
 import express from "express";
-import { deleteVideo, editVideo, getUpload, videoDetail, postUpload } from "../controller/videoController";
+import { deleteVideo, getUpload, videoDetail, postUpload, getEditVideo, postEditVideo } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
 import routes from "../routes";
 
 const videoRouter = express.Router();
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-videoRouter.get(routes.editVideo, editVideo);
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 export default videoRouter;
