513ca5cd8a396823086cfc4ec8bebeaf90318ae8
joyfuldev <wings2132@naver.com>
2021-04-09 18:34:52 +0900

    Protecting Video Routes

--- controller/userController.js
+++ controller/userController.js
@@ -76,10 +76,12 @@ export const getMe = (req, res) => {
 }
 
 export const userDetail = async (req, res) => {
-    const { params: { id } } = req;
-
+    const { 
+        params: { id } 
+    } = req;
     try {
-        const user = await User.findById(id);
+        const user = await User.findById(id).populate('videos');
+        console.log(user);
         res.render("userDetail", { pageTitle: "User Detail", user });
     } catch(error) {
         res.redirect(routes.home);
